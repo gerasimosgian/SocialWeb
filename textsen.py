faf5fa6f956e41f3864889359f4040f9a9065613
iliagou <ilia.gouma@hotmail.com>
2018-03-20 11:11:08 +0100

    Add files via upload

---
+++ textsen.py
@@ -0,0 +1,32 @@
+import json
+import csv
+from textblob import TextBlob
+
+
+#set the input and outputing file
+input_file= "tweets.json"
+output_file= "results.csv"
+
+#store all json data
+tweets_novartis = []
+
+with open (input_file) as input_novartis:
+    for line in input_novartis:
+        tweets_novartis.append(json.loads(line))
+
+#open output file to store the results
+with open(output_file, "w") as output_novartis:
+    writer = csv.writer(output_novartis)
+
+    #iterate through all the tweets
+    for tweets_novartis in tweets_novartis:
+        tweet = tweets_novartis["full_text"]
+
+        #TextBlob to calculate sentiment
+        #tweet = Textblob(tweet)
+        #tweet = tweet.replace("\n" , " ")
+        #tweet = tweet.replace("\r" , " ")
+
+        sentiment = [[TextBlob(tweet).sentiment.polarity]]
+        writer.writerows(sentiment)
+
