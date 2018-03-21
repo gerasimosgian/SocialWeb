17496adeb26a0c88bf2bbc2f54cca93eb5fee978
gerasimosgian <33956621+gerasimosgian@users.noreply.github.com>
2018-03-22 00:29:21 +0100

    Update TweetSentimentAnalysis.py

--- TweetSentimentAnalysis.py
+++ TweetSentimentAnalysis.py
@@ -1,28 +1,39 @@
+#-*- coding: utf-8 -*-
 import json
 import csv
 from textblob import TextBlob
+from textblob.translate import Translator
+import itertools 
+import simplejson
 
+input_file= "nov.json"
+output_file="results.csv"
 
-#set the input and outputing file
-input_file= "tweets.json"
-output_file= "results.csv"
-
-#store all json data
 tweets_novartis = []
 
 with open (input_file) as input_novartis:
-    for line in input_novartis:
-        tweets_novartis.append(json.loads(line))
+	for line in input_novartis:
+		tweets_novartis.append(json.loads(line))
 
-#open output file to store the results
-with open(output_file, "w") as output_novartis:
-    writer = csv.writer(output_novartis)
+with open(output_file, 'w', newline = '') as output_novartis:
+	writer = csv.writer(output_novartis)
 
-    #iterate through all the tweets
-    for tweets_novartis in tweets_novartis:
-        tweet = tweets_novartis["full_text"]
-
-        #TextBlob to calculate sentiment
-        sentiment = [[TextBlob(tweet).sentiment.polarity]]
-        writer.writerows(sentiment)
-
+	for tweets_novartis in tweets_novartis:
+		
+		tweet = tweets_novartis["full_text"]
+		lan = tweets_novartis["created_at"]
+		
+		tweet=TextBlob(tweet)
+		
+		tweet = tweet.replace("\n", " ")
+		tweet = tweet.replace("\r", " ")
+		def set_date(self, lan):
+			date = time.striptime(lan, '%b %d %Y ')
+			
+			self.date = datetime.fromtimestamp(time.mktime(date))
+		sentiment=[tweet.sentiment.polarity]
+		
+		writer.writerows(zip(sentiment, [lan[4:10], lan[26:]]))
+		
+		
+		
