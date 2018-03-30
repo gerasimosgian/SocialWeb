6a619625b85e726527edf62a92455c7b4ac661b8
gerasimosgian <33956621+gerasimosgian@users.noreply.github.com>
2018-03-30 13:32:33 +0200

    Update TweetSentimentAnalysis.py

--- TweetSentimentAnalysis.py
+++ TweetSentimentAnalysis.py
@@ -2,19 +2,21 @@
 import json
 import csv
 from textblob import TextBlob
-from textblob.translate import Translator
 import itertools 
 import simplejson
 
+#Input and output files
 input_file= "nov.json"
 output_file="results.csv"
 
 tweets_novartis = []
 
+#Append the json file
 with open (input_file) as input_novartis:
 	for line in input_novartis:
 		tweets_novartis.append(json.loads(line))
 
+#parsing the text and date data 
 with open(output_file, 'w', newline = '') as output_novartis:
 	writer = csv.writer(output_novartis)
 
@@ -23,6 +25,7 @@ with open(output_file, 'w', newline = '') as output_novartis:
 		tweet = tweets_novartis["full_text"]
 		lan = tweets_novartis["created_at"]
 		
+		#Sentiment Analysis
 		tweet=TextBlob(tweet)
 		
 		tweet = tweet.replace("\n", " ")
@@ -31,6 +34,7 @@ with open(output_file, 'w', newline = '') as output_novartis:
 			date = time.striptime(lan, '%b %d %Y ')
 			
 			self.date = datetime.fromtimestamp(time.mktime(date))
+		#Sentiment score
 		sentiment=[tweet.sentiment.polarity]
 		
 		writer.writerows(zip(sentiment, [lan[4:10], lan[26:]]))
